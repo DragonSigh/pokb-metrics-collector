535d7c0415c0adb42a8948b125012b635b37b4d5
DragonSigh <dragonsigh@gmail.com>
2023-09-26 11:40:34 +0300

    added metric 055 for analyze, some WIP on metric 019

--- pokb_metrics_collector/metric_019.py
+++ pokb_metrics_collector/metric_019.py
@@ -3,6 +3,7 @@ import utils
 import bi_emias
 import os
 import json
+import pandas as pd
 from loguru import logger
 
 first_date = config.first_date
@@ -23,18 +24,53 @@ def start_019_report_saving():
         for _units in _departments["units"]:
             bi_emias.authorize(_units["login"], _units["password"])
     if not utils.is_actual_report_exist(
-        "Мониторинг проведения телемедицинских консультаций [Пререкорд].xlsx"
+        config.reports_path
+        + "\\Мониторинг проведения телемедицинских консультаций [Пререкорд].xlsx"
     ):
         bi_emias.load_any_report("tk_cells", first_date, last_date)
         bi_emias.export_report()
-
-    bi_emias.load_any_report("tk_cells_doc_v2", first_date, last_date)
-    bi_emias.export_report()
-
-    bi_emias.load_any_report("shed_lpu", first_date, last_date)
-    bi_emias.export_report()
+    if not utils.is_actual_report_exist(
+        config.reports_path + "\\Детализированный отчёт по врачам, проводящим ТМК.xlsx"
+    ):
+        bi_emias.load_any_report("tk_cells_doc_v2", first_date, last_date)
+        bi_emias.export_report()
+    if not utils.is_actual_report_exist(config.reports_path + "\\Отчет по расписанию врачей .xlsx"):
+        bi_emias.load_any_report("shed_lpu", first_date, last_date)
+        bi_emias.export_report()
 
     logger.debug("Выгрузка из BI ЕМИАС завершена")
 
 
-start_019_report_saving()
+def analyze_019_data():
+    # Загружаем отчёты в датафреймы
+    # 1.1 http://bi.mz.mosreg.ru/#form/tk_cells
+    # (нужен для проверки использования шаблонов в кабинетах (ЛЛО, Справки онлайн и т.д.)
+    df_tk_cells = pd.read_excel(
+        config.reports_path
+        + "\\Мониторинг проведения телемедицинских консультаций [Пререкорд].xlsx",
+        skiprows=1,
+        header=0,
+    )
+    # 1.2 http://bi.mz.mosreg.ru/#form/tk_cells_doc_v2 (детализированный по врачам)
+    df_tk_cells_doc_v2 = pd.read_excel(
+        config.reports_path + "\\Детализированный отчёт по врачам, проводящим ТМК.xlsx",
+        skiprows=1,
+        header=0,
+    )
+    # 1.3 http://bi.mz.mosreg.ru/#form/shed_lpu (нужен для сопоставления врача и подразделения)
+    df_shed_lpu = pd.read_excel(
+        config.reports_path + "\\Отчет по расписанию врачей .xlsx", skiprows=1, header=0
+    )
+    # Только Подольская ОКБ
+    df_tk_cells = df_tk_cells[(df_tk_cells["ОГРН"] == 1215000036305)]
+    df_tk_cells_doc_v2 = df_tk_cells_doc_v2[(df_tk_cells_doc_v2["ОГРН"] == 1215000036305)]
+    df_shed_lpu = df_shed_lpu[(df_shed_lpu["ОГРН"] == 1215000036305)]
+    # Оставляем только врачей и подразделения для сопоставления
+    df_shed_lpu = df_shed_lpu[["ФИО врача", "Подразделение"]]
+    # 2.1 В столбце Занято не должно быть нулей
+    df_tk_cells = df_tk_cells.loc[df_tk_cells["Занято"] != 0]
+    df_tk_cells_doc_v2 = df_tk_cells_doc_v2.loc[df_tk_cells_doc_v2["Занято"] != 0]
+
+
+# start_019_report_saving()
+analyze_019_data()
